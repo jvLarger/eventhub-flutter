8307d1eff011748f3b0a9ed9a70d3ab51f410a7e
João Victor <larger13@hotmail.com>
2023-05-15 19:28:54 -0300

    const update
    const update

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Event Hub',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light,
-      home: LoginPage(),
+      home: const LoginPage(),
     );
   }
 }
